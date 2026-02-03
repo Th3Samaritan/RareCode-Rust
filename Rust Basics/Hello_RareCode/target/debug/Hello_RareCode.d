b8f42821e4f60fa18219c9be513967b6ce976d66
Th3Samaritan <abdulsamadmuyideen@gmail.com>
2026-02-03 01:37:01 +0100

    Hello Rust

---
+++ Rust Basics/Hello_RareCode/target/debug/Hello_RareCode.d
@@ -0,0 +1,1 @@
+C:\Users\MAST\Documents\RareCode-Rust\Rust\ Basics\Hello_RareCode\target\debug\Hello_RareCode.exe: C:\Users\MAST\Documents\RareCode-Rust\Rust\ Basics\Hello_RareCode\src\main.rs
---
+++ Rust Basics/Hello_RareCode/target/debug/deps/Hello_RareCode.d
@@ -0,0 +1,5 @@
+C:\Users\MAST\Documents\RareCode-Rust\Rust Basics\Hello_RareCode\target\debug\deps\Hello_RareCode.d: src\main.rs
+
+C:\Users\MAST\Documents\RareCode-Rust\Rust Basics\Hello_RareCode\target\debug\deps\Hello_RareCode.exe: src\main.rs
+
+src\main.rs:
